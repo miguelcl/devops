1280ae169d02796a9c1770a86be84292128d754c
Miguel Angel Amador <devops@motd.cl>
2018-10-08 17:22:59 -0300

    Create jenkins_install.sh

---
+++ jenkins_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#
+################################
+#
+# SCRIPT INSTALACION JENKINS
+# S.O: Centos 7.x / RHEL 7.X
+# Autor: Miguel Amador @miguelcl 
+#
+#
+################################
+
+
+
+URL_JENK="http://besp.falabella.com/core/jenkins/"
+URL_PROXY="http://user:pass@proxyfal.falabella.cl:8080"
+
+####### Descarga de informacion de repos y key
+# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+# wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+curl -O  /etc/yum.repos.d/jenkins.repo  $URL_JENK/jenkins.repo
+rpm --import 							$URL_JENK/jenkins-ci.org.key
+
+### if exist proxy set global proxy setting.
+export http_proxy=$URL_PROXY
+
+### Instalacion jenkins from rpm
+yum install java
+yum install jenkins
+
+/sbin/chkconfig jenkins on
+
+systemctl status jenkins.service
+systemctl stop jenkins.service
+systemctl start jenkins.service
+
+netstat -putan | grep jenkins
